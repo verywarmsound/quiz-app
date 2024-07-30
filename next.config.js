867f69263edfd6e8551897ff31851deb5f2dc63a
verywarmsound <imkarpacz@gmail.com>
2024-07-30 23:12:46 +0200

    build fix

--- next.config.js
+++ next.config.js
@@ -4,7 +4,6 @@ const nextConfig = {
     compiler: {
         removeConsole: process.env.NODE_ENV !== "development",
     },
-    output: 'export',
     webpack: (config, { isServer }) => {
         return config;
     },
